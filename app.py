6cf825d7ba0e42a0a0e5753106898b4c7616b528
ParthP22 <parthpatel0422@gmail.com>
2024-02-14 15:45:29 -0500

    added Pranjal's login page

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, redirect, url_for, session, request
+import sqlite3
 app = Flask(__name__)
 
-
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
+conn = sqlite3.connect("Seaview_DB.db")
+cursor = conn.cursor()
 
 @app.route('/')
-def render_page():
-    return render_template('home.html')
+def welcome():
+    return render_template('welcome.html')
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
