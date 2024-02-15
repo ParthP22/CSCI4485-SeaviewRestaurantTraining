277e08f80564763a7e090af1f13e09b5e4970c46
ParthP22 <parthpatel0422@gmail.com>
2024-02-14 23:23:38 -0500

    Set up MySQL on app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, redirect, url_for, session, request
-import sqlite3
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+# import sqlite3
 app = Flask(__name__)
 
-conn = sqlite3.connect("Seaview_DB.db")
-cursor = conn.cursor()
+# conn = sqlite3.connect("Seaview_DB.db")
+# cursor = conn.cursor()
+
+app.config['MYSQL_HOST'] = 'sql5.freesqldatabase.com'
+app.config['MYSQL_USER'] = 'sql5684251'
+app.config['MYSQL_PASSWORD'] = 'nJzuzJDJ4D'
+app.config['MYSQL_DB'] = 'sql5684251'
+
+mysql = MySQL(app)
+
 
 @app.route('/')
 def welcome():
-    return render_template('welcome.html')
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('INSERT INTO USERS (USER_NAME, PASS_WORD, EMAIL) VALUES(%s, %s, %s)', ("test","test","testgmail",))
+    cursor.execute('SELECT * FROM USERS')
+    mysql.connection.commit()
+    account = cursor.fetchone()
+    name = account['USER_NAME']
+    return render_template('welcome.html',name=name)
 
 
 
@@ -16,4 +32,6 @@ def welcome():
 
 
 if __name__ == '__main__':
+    # welcome()
+
     app.run(debug=True)
