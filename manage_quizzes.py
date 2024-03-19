baa14eede9049fa614dae50879f94fc95134dc14
Ryan Minneo <ryanminneo03@gmail.com>
2024-03-18 21:57:08 -0400

    Manage Quizzes Py File
    Created a very basic python file which basically will connect the HTML file for the Quiz List with the website and the database.

---
+++ manage_quizzes.py
@@ -0,0 +1,17 @@
+# Author(s): Ryan Minneo
+# This file contains the code that is used to manage quizzes,
+# such as being able to register new quizzes, edit existing quizzes, and delete quizzes if need be.
+
+import re
+from flask import Flask, render_template, redirect, url_for, session, request
+import database
+from __main__ import website
+
+@website.route('/manage_quizzes')
+def manage_quizzes():
+    cursor = database.conn.cursor()
+
+    cursor.execute('SELECT * FROM QUIZ')
+    quizzes = cursor.fetchall()
+    return render_template('manage_quizzes.html', quizzes = quizzes)
+
