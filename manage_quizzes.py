f5b8950980923bed1c8fbe039e54ad473bf93287
nguyenryan113 <90424783+nguyenryan113@users.noreply.github.com>
2024-03-20 20:46:48 -0400

    Quizzes can be Created
    Quizzes can be created and added to the database without questions. We need Quiz_ID in the QUESTIONS table of the database to link the questions, but once that is set up, quizzes can be created properly.
    These Quizzes cannot be viewed yet since we need to have a python file that generates a quiz page.

--- manage_quizzes.py
+++ manage_quizzes.py
@@ -19,4 +19,44 @@ def manage_quizzes():
 @website.route('/quiz_editor')
 def quiz_editor():
     cursor = database.conn.cursor()
+    return render_template('quiz_editor.html')
+
+@website.route('/quiz_editing', methods=['GET', 'POST'])
+def quiz_editing():
+    count = 0
+    # Check if
+    if request.method == 'POST' and 'quiz_name' in request.form and 'quiz_desc' in request.form:
+        # Retrieve data from the HTML form
+        quiz_name = request.form['quiz_name']
+        quiz_desc = request.form['quiz_desc']
+
+        # Retrieve questions and answers dynamically
+        questions = []
+        for key, value in request.form.items():
+            if key.startswith('question'):
+                question_number = key.replace('question', '')
+                question = {
+                    'QUESTION': value,
+                    'ANSWER_A': request.form[f'option{question_number}A'],
+                    'ANSWER_B': request.form[f'option{question_number}B'],
+                    'ANSWER_C': request.form[f'option{question_number}C'],
+                    'ANSWER_D': request.form[f'option{question_number}D'],
+                    'CORRECT_ANSWER': request.form[f'correctAnswer{question_number}']
+                }
+                count = count + 1
+                questions.append(question)
+
+        cursor = database.conn.cursor()
+
+        cursor.execute('INSERT INTO QUIZZES (QUIZ_NAME, TOTAL_QUESTIONS, EMPLOYER_ID, TOTAL_CORRECT, TOTAL_INCORRECT, IS_VISIBLE, QUIZ_DESC) VALUES (?, ?, ?, ?, ?, ?, ?)', (quiz_name, count, 1, 0, 0, 1, quiz_desc))
+
+        #for question in questions:
+        #    cursor.execute('''INSERT INTO QUESTIONS (QUESTION, ANSWER_A, ANSWER_B, ANSWER_C, ANSWER_D, CORRECT_ANSWER)
+        #                          VALUES (?, ?, ?, ?, ?, ?)''',
+        #                   (question['QUESTION'], question['ANSWER_A'], question['ANSWER_B'],
+         #                   question['ANSWER_C'], question['ANSWER_D'], question['CORRECT_ANSWER']))
+        # Commit changes to the database
+        database.conn.commit()
+
+    return render_template('manage_quizzes.html')
 
