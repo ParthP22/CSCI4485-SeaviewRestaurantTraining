a9870c3c4957367c6a58aa7e1dc487600ebbd1c4
Ryan Minneo <ryanminneo03@gmail.com>
2024-04-10 18:09:25 -0400

    Updating Session_Handling to manage Sessions better.
    Now you can use the back arrows and forward arrows on browsers properly.

--- session_handling.py
+++ session_handling.py
@@ -5,12 +5,42 @@
 
 from flask import Flask, render_template, redirect, url_for, session, request
 import database
+import datetime
 from routes import website
 
 
-@website.route('/login')
-def login():
+@website.route('/show_login')
+def show_login():
     return render_template('index.html')
+@website.route('/login', methods=['POST'])
+def login():
+    # Initialize message variable
+    msg = ''
+    # Create variables for easy access
+    username = request.form['username']
+    password = request.form['password']
+    # Check if account exists using SQLite
+    cursor = database.conn.cursor()
+    cursor.execute('SELECT * FROM Users WHERE Username=? AND Password=?', (username, password,))
+    account = cursor.fetchone()
+    # If account exists in the database
+    if account:
+        session['id'] = account[0]
+        session['logged_in'] = True
+        session['username'] = username
+        session['password'] = password
+        session['role'] = account[6]
+        session['restricted'] = account[8]
+
+        if session['restricted'] == 1:
+            msg = 'Account is restricted'
+            return render_template('index.html', msg=msg)
+
+        # After successful login, redirect to dashboard
+        return redirect(url_for('authenticate_user'))
+    else:
+        msg = 'Incorrect username/password!'
+        return render_template('index.html', msg=msg)
 
 @website.route('/welcome', methods=['GET', 'POST'])
 def logout():
@@ -91,7 +121,10 @@ def render_employee_dashboard(account, cursor):
         else:
             num_incorrect.append(0)
 
-    return render_template('employee_dashboard.html', progress=total_correct, total_questions=total_questions, quizzes=quizzes, percent=percent, num_correct=num_correct, num_incorrect=num_incorrect, quiz_list=quiz_list)
+    current_datetime = datetime.datetime.now()
+    formatted_current_datetime = current_datetime.strftime("%Y-%m-%dT%H:%M:%S")
+
+    return render_template('employee_dashboard.html', progress=total_correct, total_questions=total_questions, quizzes=quizzes, percent=percent, num_correct=num_correct, num_incorrect=num_incorrect, quiz_list=quiz_list, current_date = formatted_current_datetime)
 
 @website.route('/dashboard', methods=['GET', 'POST'])
 def authenticate_user():
@@ -99,43 +132,15 @@ def authenticate_user():
     # Check if "username" and "password" POST requests exist (user submitted form)
     account = None
     cursor = database.conn.cursor()
-    if 'logged_in' in session:
+    if 'logged_in' in session and session['logged_in']:
         cursor.execute('SELECT * FROM Users WHERE Username=? AND Password=?', (session['username'], session['password']))
         account = cursor.fetchone()
-        if account[6]  == 1:
+        if account and account[6] == 1:
             return render_template('manager_dashboard.html')
         else:
             return render_employee_dashboard(account, cursor)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        # Check if account exists using SQLite
-        cursor.execute('SELECT * FROM Users WHERE Username=? AND Password=?', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        # If account exists in accounts table in out database
-        if account:
-            session['id'] = account[0]
-            session['logged_in'] = True
-            session['username'] = username
-            session['password'] = password
-            session['role'] = account[6]
-            session['restricted'] = account[8]
-            if session['restricted'] == 1:
-                msg = 'Account is restricted'
-                return ('Account is restricted')
-            if session['role'] == 1:
-                #Admin
-                return render_template('manager_dashboard.html', msg=msg)
-            else:
-                #employee/basic user page
 
-                return render_employee_dashboard(account,cursor)
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
-    return render_template('index.html', msg=msg)
+    return redirect(url_for('login'))
 
 
